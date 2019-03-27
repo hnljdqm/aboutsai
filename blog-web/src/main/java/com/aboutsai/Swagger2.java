006bbf4a38601aaf57d366a34e48d026440c4547
sai <hnljdqm@163.com>
2019-03-28 00:07:59 +0800

    修改依赖包

--- blog-web/src/main/java/com/aboutsai/Swagger2.java
+++ blog-web/src/main/java/com/aboutsai/Swagger2.java
@@ -2,6 +2,7 @@ package com.aboutsai;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -16,7 +17,6 @@ import springfox.documentation.spring.web.plugins.Docket;
  */
 @Configuration
 public class Swagger2 {
-
 	@Bean
 	public Docket createRestApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
@@ -26,13 +26,14 @@ public class Swagger2 {
 				.paths(PathSelectors.any())
 				.build();
 	}
-	
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder()
+    
+    //构建 api文档的详细信息函数,注意这里的注解引用的是哪个
+    private ApiInfo apiInfo() {
+    	return new ApiInfoBuilder()
 				.title("springboot利用swagger构建api文档")
 				.description("简单优雅的restful风格，http://blog.csdn.net/saytime")
 				.termsOfServiceUrl("http://blog.csdn.net/saytime")
 				.version("1.0")
 				.build();
-	}
+    }
 }
